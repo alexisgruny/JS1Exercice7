6b7b3f6b24c48a1779f0cb7977f691d9a935baf8
AlexisG <alexis.gruny60@gmail.com>
2018-06-20 13:44:09 +0200

    ajout de condition

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ function bizarre(){
   var x = document.getElementById("shoeSize").value;
   var y = document.getElementById("yearOfBirth").value;
   var a;
+  if (x && y != 'string'){
+  alert("entrez un nombre s'il vous plait")
+  }
+  else{
+
   a = x * 2;
   a = a + 5;
   a = a * 50;
   a = a - y;
   a = a + 1766;
   alert(a);
+  }
 }
