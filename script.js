e37bd229a6326987d08689d2a6f49fd39f7a4b72
AlexisG <alexis.gruny60@gmail.com>
2018-06-21 08:46:05 +0200

    ajout commentaire

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ function bizarre(){
   var shoeSize = document.getElementById("shoeSize").value;
   var yearOfBirth = document.getElementById("yearOfBirth").value;
   var resultat;
+  // test regex
   if (/^[0-9]+$/.test(shoeSize && yearOfBirth) == true);{
+  // suite de calcul
   resultat = shoeSize * 2;
   resultat = resultat + 5;
   resultat = resultat * 50;
   resultat = resultat - yearOfBirth;
   resultat = resultat + 1766;
+  // affichage du resultat
   alert(resultat);
   }
 }
